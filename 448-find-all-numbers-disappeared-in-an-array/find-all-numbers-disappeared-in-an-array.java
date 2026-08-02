438ee2f719b82447afe618f72997beb254cfabec
MD TAHSIN AHMAD <t3616639@gmail.com>
2026-08-02 17:40:41 +0530

    Time: 6 ms (82.95%) | Memory: 67.1 MB (74.21%) - LeetSync

---
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] arr) {
+        int i = 0;
+        while(i < arr.length){
+            int correct = arr[i] - 1;
+            if(arr[i] != arr[correct]){
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+        // finding all the missing numbers 
+        List<Integer> ans = new ArrayList<>();
+        for(int index = 0; index < arr.length; index++){
+            if(arr[index] != index + 1){
+                ans.add(index + 1);
+            }
+
+        }
+
+        return ans;
+
+        
+    }
+}
