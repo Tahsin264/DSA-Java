bece1438c803717f194ac1bf34f3d3c3d2b384c5
MD TAHSIN AHMAD <t3616639@gmail.com>
2026-09-19 13:25:35 +0530

    Time: 2 ms (91.67%) | Memory: 47.8 MB (38.16%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,20 +1,29 @@
 class Solution {
     public void moveZeroes(int[] arr) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        int n = arr.length;
-        for(int i = 0 ; i < n ; i++){
-            if(arr[i]!=0){
-                temp.add(arr[i]);
+         int j = -1;
+         for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i] == 0){
+                j = i;
+                break;
+
             }
+         }
 
-        }
+         if(j == -1){
+            return;
+         }
 
-        for(int i = 0 ; i < temp.size(); i++){
-            arr[i] = temp.get(i);
-        }
-        int k = temp.size();
-        for(int i = k; i < n ; i++){
-            arr[i] = 0;
-        }
+         for(int i = j+1; i<arr.length ; i++){
+            if(arr[i] != 0){
+                swap(arr , i , j);
+                j++;
+            }
+         }
+    }
+
+    static void swap(int arr[] , int s,int e){
+        int temp = arr[s];
+        arr[s] = arr[e];
+        arr[e] = temp;
     }
 }
