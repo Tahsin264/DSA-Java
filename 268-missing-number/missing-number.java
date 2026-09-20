7abc8288f573ecdd4b4dad7a60581538017af578
MD TAHSIN AHMAD <t3616639@gmail.com>
2026-09-20 17:01:56 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (48.73%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,26 +1,16 @@
 class Solution {
     public int missingNumber(int[] arr) {
-        int i = 0;
-        while(i < arr.length){
-            int correct = arr[i];
-            if(arr[i] < arr.length && arr[i] != arr[correct]){
-                int temp = arr[i];
-                arr[i] = arr[correct];
-                arr[correct] = temp;
-            }
-            else{
-                i++;
-            }
+        int n = arr.length;
+        int missingno;
+        int sum1 = n * (n+1)/2; 
+        int sum2 = 0;
+        for(int i = 0; i < n; i++){
+            sum2 = sum2 + arr[i];
+           
+        }
+        missingno = sum1 - sum2;
 
-            // finding the missing number
-            
-        }
-        for(int index = 0; index < arr.length ; index++){
-            if(arr[index] != index){
-                return index;
-            }
-        }
-        return arr.length;
+        return missingno;
         
     }
 
